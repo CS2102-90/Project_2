009a19da3c1f96cabd2b585657cff41c2acf2ef9
Tra Quang Minh Thong <tqmthong@gmail.com>
2022-11-11 10:35:32 +0800

    add user dummy data

---
+++ test/add_user.sql
@@ -0,0 +1,103 @@
+CALL add_user('e2222@u.nus.edu','Iron Man','DBS10232','DBS28391',
+			  'Manhatthan','237','1238123','United States','BACKER');
+CALL add_user('e23439@u.nus.edu','Spider Man','DBSdf10232','DBS28391',
+			  'Johor','2307','145238123','Singapore','BOTH');
+CALL add_user('e222122@u.nus.edu','Captain America','DBSas10232','DBS283df91',
+			  'Ulu Padan','27','12123','China','CREATOR');
+CALL add_user('e22242@u.nus.edu','One Punch Man','DBS10232','DBS28391',
+			  'Kent Ridge','28/4','1238as123','Russia','BACKER');
+CALL add_user('e222762@u.nus.edu','Superman','DBS102320','DBS28391',
+			  'Wellington','237','1238123','Germany','CREATOR');
+CALL add_user('e222202@u.nus.edu','Batman','DBS1023122','DBS283191',
+			  'Moon','2a37','1d2381g23','Japan','BOTH');
+
+CALL add_user('e221222@u.nus.edu','Captain Marvel','DBS1210232','',
+			  'Dover','237','1238123','United States','BACKER');
+CALL add_user('e234539@u.nus.edu','Ronaldo','DBSdf10232','DBS28391',
+			  'Clementi','2307','14532238123','Singapore','BOTH');
+CALL add_user('e22212322@u.nus.edu','Messi','DBSas10232','DBS283df91',
+			  'Jurong','27','12123','China','CREATOR');
+CALL add_user('e22223442@u.nus.edu','Chapin','DBS10232','',
+			  'Howards','28/4','12382123','Russia','BACKER');
+CALL add_user('e2262762@u.nus.edu','Trump','DBS1ga02320','DBS28391',
+			  'Hogwarts','237','1238ad123','Germany','CREATOR');
+CALL add_user('e22232202@u.nus.edu','Doctor Strange','DBS10a23122','',
+			  'London','df2a37','1d23812ads3','Japan','BOTH');
+
+CALL add_user('e222542@u.nus.edu','Haweye','DBS102ad32','DBS28391',
+			  'Beijing','237','1238ga123','United States','BACKER');
+CALL add_user('e2362439@u.nus.edu','Elizabeth','DBSdfads10232','',
+			  'Fuzhou','2307','14523adf8123','Singapore','BOTH');
+CALL add_user('e21222122@u.nus.edu','Huston','DBSas10232','',
+			  'Funan','27','12123','China','CREATOR');
+CALL add_user('e22123242@u.nus.edu','John Cena','DBS1g0232','DBS28391',
+			  'Neustraße','218/4','123aas8123','Russia','BACKER');
+CALL add_user('e22243762@u.nus.edu','Pham Minh Duc','DBS102a320','DBS28391',
+			  'Hilton','237','1238123','Germany','CREATOR');
+CALL add_user('e22215202@u.nus.edu','Li Chong Wei','DBS1023122','DBS283191',
+			  'Clinton','2a37','1d238123','Japan','BOTH');
+
+CALL add_user('e2224312@u.nus.edu','Lin Dan','DBS10235g2','DBS28391',
+			  'Haiphong','237','1238123','United States','BACKER');
+CALL add_user('e2345139@u.nus.edu','Manuel Neuer','DBSdf102g322','DBS28391',
+			  'Hanoi','2307','14523fda8123','Singapore','BOTH');
+CALL add_user('e26122122@u.nus.edu','BTS','DBS4as10232','',
+			  'Bangkok','27','1212asd3','China','CREATOR');
+CALL add_user('e2205242@u.nus.edu','Kim Jong Un','DBS14adsa0232','DBS28391',
+			  'Don meunang','28/4','1238123ads','Russia','BACKER');
+CALL add_user('e2227562@u.nus.edu','Warwick','DBS1023220','',
+			  'Darius','237','1238123','Germany','CREATOR');
+CALL add_user('e22220662@u.nus.edu','Sauron','DBS1023ad1242','DBS283191',
+			  'Morderkaiser','2a37','1d238123','Mordor','BOTH');
+
+CALL add_user('e2222664@u.nus.edu','Galadriel','DBS1023122','',
+			  'Gumayusi','237','1238123','United States','BACKER');
+CALL add_user('e2343915@u.nus.edu','Gandalf','DBSdf102342','DBS28391',
+			  'Moveit','2307','145238123','Singapore','BOTH');
+CALL add_user('e2221294562@u.nus.edu','Captain America','DBS23as10232','',
+			  'Elon Musk','27','12123','China','CREATOR');
+CALL add_user('e2224254@u.nus.edu','Dumbledore','DBS1021232','DBS28391',
+			  'Hacker','28/4','1238123','Russia','BACKER');
+CALL add_user('e22276245@u.nus.edu','Harry Potter','DBS10232ds0','DBS28391',
+			  'ThreeFourth','237','1238123','Germany','CREATOR');
+CALL add_user('e25622202@u.nus.edu','Harry Maguire','DBS1023122','',
+			  'Manutd','2a37','1d238123','Japan','BOTH');
+
+CALL add_user('e222052@u.nus.edu','Iron','DBS10232','DBS28391',
+			  'Manhatthan','237','1238123','United States','BACKER');
+CALL add_user('e2345439@u.nus.edu','Spider','DBSdf10232','DBS28391',
+			  'Johor','2307','145238123','Singapore','BOTH');
+CALL add_user('e222125962@u.nus.edu','Captain','DBSas10232','DBS283df91',
+			  'The force','27','12123','China','CREATOR');
+CALL add_user('e2224782@u.nus.edu','One Punch','DBS10232','DBS28391',
+			  'Kent Ridge','28/4','1238123','Russia','BACKER');
+CALL add_user('e22276462@u.nus.edu','Hieu','DBS102320','DBS28391',
+			  'Wellington','237','1238123','Germany','CREATOR');
+CALL add_user('e2222042@u.nus.edu','Ba','DBS1023122','DBS283191',
+			  'Moon','2a37','1d238123','Japan','BOTH');
+
+CALL add_user('e222122@u.nus.edu','Iron Man','DBS10232','DBS28391',
+			  'Manhatthan','22337','12381as23','United States','BACKER');
+CALL add_user('e2343129@u.nus.edu','Biang Biang mian','DBSdf10232','DBS28391',
+			  'Johor','2307','145238123','Singapore','BOTH');
+CALL add_user('e222122132@u.nus.edu','Bing','DBSas10232','DBS283df91',
+			  'Ulu Padan','2127','12123','China','CREATOR');
+CALL add_user('e222424@u.nus.edu','Jinx','DBS10232','DBS28391',
+			  'Kent Ridge','28/4','1238123','Russia','BACKER');
+CALL add_user('e222765642@u.nus.edu','John Wick','DBS102320','DBS28391',
+			  'Wellington','237','1238123','Germany','CREATOR');
+CALL add_user('e222202465@u.nus.edu','Naruto','DBS1023122','DBS283191',
+			  'Moon','2a37','1d238123','Japan','BOTH');
+
+CALL add_user('e2222456@u.nus.edu','Maradona','DBS10232','DBS28391',
+			  'Manhatthan','23347','123812ag3','United States','BACKER');
+CALL add_user('e23439546@u.nus.edu','Moukoko','DBSdf10232','DBS28391',
+			  'Johor','2307','1452381223','Singapore','BOTH');
+CALL add_user('e2221242@u.nus.edu','Maokai','DBSas10232','DBS283df91',
+			  'Sillence','27','1212ad3','China','CREATOR');
+CALL add_user('e22248672@u.nus.edu','Xin Zhao','DBS10232','DBS28391',
+			  'Voldemore','28/4','1238123','Russia','BACKER');
+CALL add_user('e22276287@u.nus.edu','Faker','DBS102320','DBS28391',
+			  'Bill','237','123812daf3','Germany','CREATOR');
+CALL add_user('e222205462@u.nus.edu','Gangplank','DBS1023122','DBS283191',
+			  'Sun','2a37','1d23812323','Japan','BOTH');
