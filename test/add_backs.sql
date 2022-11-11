a6cde12602df16f5896fa1a9e517efc568cf23ba
Tra Quang Minh Thong <tqmthong@gmail.com>
2022-11-12 01:25:58 +0800

    add dummy data backs to test on trig 3 and trig 5

---
+++ test/add_backs.sql
@@ -0,0 +1,53 @@
+INSERT INTO Backs VALUES('e2222@u.nus.edu','bronze',0,
+						'2023-3-4','2030-3-4',1500);
+INSERT INTO Backs VALUES('e2222@u.nus.edu','bronze',1,
+						'2023-3-4','2030-3-4',1500);
+INSERT INTO Backs VALUES('e2222@u.nus.edu','bronze',2,
+						'2023-3-4','2030-3-4',1500);
+INSERT INTO Backs VALUES('e2222@u.nus.edu','bronze',3,
+						'2023-3-4','2030-3-4',1500);
+INSERT INTO Backs VALUES('e2222@u.nus.edu','bronze',4,
+						'2023-3-4','2030-3-4',1500);
+INSERT INTO Backs VALUES('e2222@u.nus.edu','bronze',5,
+						'2023-3-4','2030-3-4',1500);
+
+INSERT INTO Backs VALUES('e222542@u.nus.edu','silver',0,
+						'2023-3-4','2023-3-6',2300);
+INSERT INTO Backs VALUES('e22123242@u.nus.edu','gold',0,
+						'2023-3-4','2023-3-7',3600);
+INSERT INTO Backs VALUES('e2224312@u.nus.edu','bronze',1,
+						'2023-3-4','2024-3-4',2000);
+INSERT INTO Backs VALUES('e222052@u.nus.edu','bronze',1,
+						'2023-3-4','2030-3-4',3000);
+INSERT INTO Backs VALUES('e2222042@u.nus.edu','silver',1,
+						'2023-3-4','2030-3-4',4000);
+INSERT INTO Backs VALUES('e2224782@u.nus.edu','bronze',2,
+						'2023-3-4','2030-3-4',5000);
+INSERT INTO Backs VALUES('e2343129@u.nus.edu','gold',2,
+						'2023-3-4','2030-3-4',5000);
+INSERT INTO Backs VALUES('e22123242@u.nus.edu','bronze',4,
+						'2023-3-4','2030-3-4',4000);
+INSERT INTO Backs VALUES('e22242@u.nus.edu','platinum',4,
+						'2023-3-4','2030-3-4',10000);
+INSERT INTO Backs VALUES('e234539@u.nus.edu','platinum',4,
+						'2023-3-4','2030-3-4',10000);
+INSERT INTO Backs VALUES('e2224312@u.nus.edu','bronze',5,
+						'2023-3-4','2030-3-4',1000);
+INSERT INTO Backs VALUES('e25622202@u.nus.edu','silver',6,
+						'2023-3-4','2030-3-4',2699);
+INSERT INTO Backs VALUES('e2224254@u.nus.edu','bronze',7,
+						'2023-3-4','2030-3-4',5000);
+INSERT INTO Backs VALUES('e222052@u.nus.edu','bronze',7,
+						'2023-3-4','2030-3-4',5000);
+INSERT INTO Backs VALUES('e222424@u.nus.edu','bronze',8,
+						'2023-3-4','2030-3-4',500);
+INSERT INTO Backs VALUES('e2362439@u.nus.edu','bronze',8,
+						'2023-3-4','2030-3-4',600);
+INSERT INTO Backs VALUES('e22215202@u.nus.edu','bronze',8,
+						'2023-3-4','2030-3-4',7000);
+INSERT INTO Backs VALUES('e22232202@u.nus.edu','gold',8,
+						'2023-3-4','2030-3-4',5000);
+INSERT INTO Backs VALUES('e2345139@u.nus.edu','silver',9,
+						'2023-3-4','2030-3-4',200);
+INSERT INTO Backs VALUES('e2224254@u.nus.edu','gold',9,
+						'2023-3-4','2030-3-4',5000);
