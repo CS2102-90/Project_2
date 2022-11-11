3cb01b575b8d48aa0ead20c3374aba943fc4c532
Tra Quang Minh Thong <tqmthong@gmail.com>
2022-11-12 00:15:46 +0800

    push dummy database

--- test/add_project.sql
+++ test/add_project.sql
@@ -1,76 +1,95 @@
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
+CALL add_project(0, 'e2221223@u.nus.edu','Tech',
+				 '2022-11-10', 'Smurf', '2023-11-10', 
+				 10000, ARRAY['bronze','silver','gold'],ARRAY[1000,2000,3000]);
+CALL add_project(1, 'e222202@u.nus.edu','Nuclear',
+				 '2022-11-10', 'Batman Car', '2024-11-10', 
+				 10000, ARRAY['bronze','silver','gold'],ARRAY[2000,4000,6000]);
+CALL add_project(2, 'e222762@u.nus.edu','Music',
+				 '2022-11-10', 'Hallelujah', '2025-11-10', 
+				 10000, ARRAY['bronze','silver','gold'],ARRAY[1000,2000,3000]);
+CALL add_project(3, 'e22243762@u.nus.edu','Game',
+				 '2022-11-10', 'League of Legends', '2026-11-10', 
+				 10000, ARRAY['bronze','silver','gold'],ARRAY[2000,4000,6000]);
+CALL add_project(4, 'e2345139@u.nus.edu','Education',
+				 '2022-11-10', 'Tsinghua daxue', '2027-11-10', 
+				 10000, ARRAY['bronze','silver','gold','platinum'],ARRAY[2000,5000,7000,10000]);
+CALL add_project(5, 'e2343129@u.nus.edu','Oil',
+				 '2022-11-10', 'Afghanistan', '2028-11-10', 
+				 10000, ARRAY['bronze','silver','gold'],ARRAY[1000,2000,3000]);
+CALL add_project(6, 'e2222042@u.nus.edu','Cuisine',
+				 '2022-11-10', 'Laksa', '2029-11-10', 
+				 10000, ARRAY['bronze','silver','gold'],ARRAY[1000,2000,3000]);
+CALL add_project(7, 'e2227562@u.nus.edu','Youtube',
+				 '2022-11-10', 'Dr.Who?', '2032-11-10', 
+				 10000, ARRAY['bronze','silver','gold'],ARRAY[500,2000,3000]);
+CALL add_project(8, 'e22243762@u.nus.edu','Maps',
+				 '2022-11-10', 'Google map', '2033-11-10', 
+				 10000, ARRAY['bronze','silver','gold'],ARRAY[400,2000,3000]);
+CALL add_project(9, 'e2362439@u.nus.edu','Travel',
+				 '2022-11-10', 'Stay at home', '2045-11-10', 
+				 10000, ARRAY['bronze','silver','gold'],ARRAY[100,200,300]);                                                                                                      
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
 
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
 
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
 
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
 
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
 
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
-CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
+-- CALL add_project();
